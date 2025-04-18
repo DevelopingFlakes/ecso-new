d586cfc9045856a04d6fab70bd0c0111cb3472d9
DevelopingFlakes <blackhostsimba@gmail.com>
2025-04-18 13:34:07 +0200

    Commit

---
+++ imprint.php
@@ -0,0 +1,85 @@
+<?php include '_def/head.php'; ?>
+<?php include '_def/header.php'; ?>
+
+<section class="hero-service-wrap hero-section-wrap hero-portfolio-wrap hero-faq-wrap">
+    <div class="hero-section-content-wrap">
+        <div class="custom-container">
+            <div class="hero-portfolio-body">
+                <div class="hero-section-content text-center">
+                    <br><br><br><br><br><br><br><br>
+                    <h1 class="section-title fade-in">
+                        Imprint
+                    </h1>
+                    <p>
+                        Last Updated: 18.04.2025
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="company-service-area">
+    <div class="custom-container">
+        <div class="hero-service-about">
+
+            <div class="section-header d-flex align-items-center justify-content-between w-full">
+                <div class="left">
+                    <h1 class="section-title">Company</h1>
+                </div>
+            </div>
+
+            <div class="hero-service-about-body">
+                <p>
+                    <?= nl2br("ECSO.cloud
+                    An d. Klasgaß 4
+                    56218
+                    Mülheim-Kärlich
+                    
+                    Tel.: +49 156 79 082983
+                    E-Mail: support@ecso.cloud
+                    
+                    VAT Reg. No. DE359819504
+                    CEO: Valentin Martin, Dominik Bitiqi, Kevin Echterhoff, Robin Holl
+                    "); ?>
+                </p>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<section class="company-service-area">
+    <div class="custom-container">
+        <div class="hero-service-about">
+
+            <div class="section-header d-flex align-items-center justify-content-between w-full">
+                <div class="left">
+                    <h1 class="section-title">Online Dispute Resolution</h1>
+                </div>
+            </div>
+
+            <div class="hero-service-about-body">
+                <p>
+                    <?= nl2br("Online Dispute Resolution in accordance with Art. 14, para 1 of the EU Online Dispute Resolution Regulations:
+
+The European Commission has established a platform for online dispute resolution (ODR). You can visit the platform at http://ec.europa.eu/consumers/odr . We are neither willing nor obligated to participate in a dispute resolution procedure before a consumer arbitration board.
+
+Contact Point According to Regulation (EU) 2021/784 of the European Parliament and of the Council:
+
+This email is exclusively for communication purposes in accordance with Regulation (EU) 2021/784. Other requests will not be answered.
+E-Mail: tco@ecso.cloud ; Language of communication: German, English
+
+Contact Point for authorities according to Regulation (EU) 2022/2065 of the European Parliament and of the Council:
+
+Requests from authorities that are not requests under Regulation (EU) 2021/784 of the European Parliament and of the Council (see above) can be sent to the following email address, unless other contact channels have already been established:
+E-Mail: dsa@ecso.cloud ; Language of communication: German, English
+                "); ?>
+                </p>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<?php include '_def/footer.php'; ?>
